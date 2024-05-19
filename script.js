02337444f04ee6fc9013b3d209be3c36f0f7d172
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-19 19:17:30 -0400

    converting response to json

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 fetch("https://www.colourlovers.com/api/palettes/top?numResults=20")
-    .then(response => console.log(response))
+    .then(response => response.json())
     .catch(error => console.log(error));
