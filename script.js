c05d20bdb04b98e0fcbc5c696444df828df676c0
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-19 19:19:44 -0400

    data to console log

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 fetch("https://www.colourlovers.com/api/palettes/top?numResults=20")
     .then(response => response.json())
+    .then(data => console.log(data))
     .catch(error => console.log(error));
