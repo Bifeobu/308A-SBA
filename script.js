a3857b07dcefa203f2473e81767b0c859034e616
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-20 07:31:17 -0400

    console log data

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ fetch("https://jsonplaceholder.typicode.com/photos")
     .then(response => {
         return response.json();
     })
-    // .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+    })
+    
     // .catch(error => console.log(error));
