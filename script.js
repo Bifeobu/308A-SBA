1f919c4330f643ae4b410df9850ff8c3a1b12deb
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-20 07:21:06 -0400

    changed to json

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 fetch("https://jsonplaceholder.typicode.com/photos")
-    .then(response => {console.log(response)})
+    .then(response => {
+        return response.json();
+    })
     // .then(data => console.log(data))
     // .catch(error => console.log(error));
