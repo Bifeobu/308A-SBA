2b6bb029b336f8fc1b53aa857b5e3ec5d538d5af
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-21 01:11:16 -0400

    used first fetch

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /////////////using fetch for external data///////////////
-// fetch("https://jsonplaceholder.typicode.com/albums")
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
+fetch("https://jsonplaceholder.typicode.com/albums")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
     
-//     .catch(error => {
-//         console.log(error)
-//     });
+    .catch(error => {
+        console.log(error)
+    });
 
 /////////////////using async and await (1st)///////////////
 // fetchAlbums();
@@ -31,19 +31,6 @@
 //     }
 // };
 // fetchAlbums();
-
-async function fetchAlbums(){
-        const albumName = document.getElementById("albumName").value.toLowerCase();
-        const response = await fecth("https://jsonplaceholder.typicode.com/albums");
-
-        if(!response.ok){
-            throw new Error("Data does not exist");
-        }
-
-        const data = await response.json();
-        console.log(data);
-};
-fetchAlbums();
 
 /////////////////using async and await (2nd)///////////////
 // async function fetchAlbums() {
