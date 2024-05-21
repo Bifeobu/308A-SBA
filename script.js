a22d9a45b0e688cae52bfc92f52762281e18577c
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-05-20 22:14:54 -0400

    making use of async

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-fetch("https://jsonplaceholder.typicode.com/albums")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
+// fetch("https://jsonplaceholder.typicode.com/albums")
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
     
-    .catch(error => {
-        console.log(error)
-    });
+//     .catch(error => {
+//         console.log(error)
+//     });
+
+async function fetchAlbums(){
+    try{
+        const response = await fecth("https://jsonplaceholder.typicode.com/albums")
+
+        if(!response.ok){
+            throw new Error("Data does not exist");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
